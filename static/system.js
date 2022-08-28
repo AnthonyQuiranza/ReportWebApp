53ee4c29db260a958250e349b25c03c7a8d91ef2
Anthony <anthony.quiranza@upec.edu.ec>
2022-08-28 16:03:37 -0500

    static files

---
+++ static/system.js
@@ -0,0 +1,5 @@
+function alerta() {
+    console.log("La consola esta funcionando");
+    toastr.success('Registro correcto, se ha enviado un código de verificación a tu correo electrónico.',"CITAS CUBA");
+}
+
