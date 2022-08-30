caebeee88f531e974f246fd818a80d5fadfa178e
Anthony <anthony.quiranza@upec.edu.ec>
2022-08-29 19:25:27 -0500

    Hora de cita

--- reportapp/settings.py
+++ reportapp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hz4=1num%@^4_0n7h+%7i@fzid%@o$@wkqi%jnm#8nb_9pmgty'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
